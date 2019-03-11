0dfb9e56105e883fb874c2e019504a2651176edf
Lisa Ong <issohl@nus.edu.sg>
2019-03-11 16:56:42 +0800

    cleanup

--- day1/kapua.py
+++ day1/kapua.py
@@ -1,7 +1,4 @@
 # Generated python client
-import time
-
-# Generated python client
 import swagger_client
 from swagger_client.rest import ApiException
 from pprint import pprint
@@ -13,8 +10,14 @@ api_instance = swagger_client.AuthenticationApi()
 body = swagger_client.ApiKeyCredentials(api_key='12345678kapua-password') # ApiKeyCredentials | The API KEY authentication credential of a user.
 
 try:
-    # Authenticate an user
+    # Authenticate an API user
     api_response = api_instance.authentication_api_key(body)
-    print(api_response)
+    pprint(api_response)
+
+    # Get the token id for future requests
+    token_id = api_response.token_id
+
+    # Get 
+
 except ApiException as e:
     print("Exception when calling AuthenticationApi->authentication_api_key: %s\n" % e)
