83d59ee180bfa0b93dfa5d6d47625b1693b1d372
Lisa Ong <issohl@nus.edu.sg>
2019-03-14 15:01:17 +0800

    wip: grant permissions

--- day1/kapua_accounts.py
+++ day1/kapua_accounts.py
@@ -22,7 +22,6 @@ def create_account(name, organization, email):
         api_response = api_instance.account_create(scope_id, body)
         pprint(api_response)
         print(f'Created account with id: {api_response.id}')
-        return api_response.id
 
     except ApiException as e:
         print("Exception when calling API: %s\n" % e)
@@ -32,9 +31,7 @@ def get_account_id(name):
     try:
         api_client = kapua_config.get_api_client()
         api_instance = swagger_client.AccountsApi(api_client)
-
         api_response = api_instance.account_simple_query(scope_id, name=name, limit=1)
-        pprint(api_response)
 
         if len(api_response.items) > 0:
             account_id = api_response.items[0].id
@@ -74,6 +71,46 @@ def create_user(account_id, name):
     except ApiException as e:
         print("Exception when calling API: %s\n" % e)
 
+def grant_permissions(account_id, user_id):
+    try:
+        api_client = kapua_config.get_api_client()
+        api_instance = swagger_client.AccessInfoApi(api_client)
+
+        # create access info
+        body = swagger_client.AccessInfoCreator(user_id=user_id)
+        api_response = api_instance.access_info_create(account_id, body)
+        pprint(api_response)
+
+        # add access permission
+        access_info_id = api_response.id
+        permission = swagger_client.Permission(target_scope_id=account_id)
+        body = swagger_client.AccessPermissionCreator(permission=permission)
+        api_response = api_instance.access_permission_create(account_id, access_info_id, body)
+        pprint(api_response)
+
+    except ApiException as e:
+        print("Exception when calling API: %s\n" % e)    
+
+def list_permissions(account_id, user_id):
+    try:
+        api_client = kapua_config.get_api_client()
+        api_instance = swagger_client.AccessInfoApi(api_client)
+
+        body = swagger_client.AccessInfoQuery()
+        api_response = api_instance.access_info_query(account_id, body)
+        print('AccessInfo:')
+        pprint(api_response)
+        access_info_id = api_response.items[0].id
+
+        body = swagger_client.AccessPermissionQuery()
+        api_response = api_instance.access_permission_query(account_id, access_info_id, body)
+        print('Permissions:')
+        pprint(api_response)
+
+    except ApiException as e:
+        print("Exception when calling API: %s\n" % e)    
+
+
 def delete_user(account_id, name):
     try:
         api_client = kapua_config.get_api_client()
@@ -91,22 +128,6 @@ def delete_user(account_id, name):
     except ApiException as e:
         print("Exception when calling API: %s\n" % e)
 
-def list_users(account_id=None):
-    if not account_id:
-        account_id = kapua_config.get_scope_id()
-
-    try:
-        api_client = kapua_config.get_api_client()
-        api_instance = swagger_client.UsersApi(api_client)
-
-        body = swagger_client.UserQuery()
-
-        api_response = api_instance.user_query(account_id, body)
-        pprint(api_response)
-
-    except ApiException as e:
-        print("Exception when calling API: %s\n" % e)
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Automates creation of Kapua accounts and users')
     parser.add_argument('command', type=str, choices=['add_acc', 'add_user', 'delete_user', 'delete_all'])
@@ -119,15 +140,16 @@ if __name__ == '__main__':
         'email':'diec1@iss.edu',
         'org':'iss'
     }
-    user = 'user1'
+    user = 'user2'
 
     if args.command == 'add_acc':
         create_account(name=account['name'], organization=account['org'], email=account['email'])
     else:
         account['id'] = get_account_id(name=account['name'])
         if args.command == 'add_user':
-            create_user(account_id=account['id'], name=user)
-            list_users(account_id=account['id'])
+            user_id = create_user(account_id=account['id'], name=user)
+            grant_permissions(account_id=account['id'], user_id=user_id)
+            list_permissions(account_id=account['id'], user_id=user_id)
         elif args.command == 'delete_user':
             delete_user(account_id=account['id'], name=user)
         elif args.command == 'delete_all':
