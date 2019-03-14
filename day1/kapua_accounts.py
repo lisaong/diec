d89fab3c5cdcd680ab19adda92ee759a01f720f2
Lisa Ong <issohl@nus.edu.sg>
2019-03-14 13:53:05 +0800

    account id is scope id

--- day1/kapua_accounts.py
+++ day1/kapua_accounts.py
@@ -19,55 +19,78 @@ def create_account(name, organization, email):
         # Create an Account
         api_response = api_instance.account_create(scope_id, body)
         pprint(api_response)
+        print(f'Created account with id: {api_response.id}')
+        return api_response.id
 
     except ApiException as e:
         print("Exception when calling API: %s\n" % e)
 
-def list_accounts():
+def get_account_id(name):
     scope_id = kapua_config.get_scope_id()
     try:
         api_client = kapua_config.get_api_client()
         api_instance = swagger_client.AccountsApi(api_client)
 
-        api_response = api_instance.account_simple_query(scope_id, limit=1)
+        api_response = api_instance.account_simple_query(scope_id, name=name, limit=1)
         pprint(api_response)
+
+        if len(api_response.items) > 0:
+            account_id = api_response.items[0].id
+            print(f'Found account id: {account_id} for {name}')
+            return account_id
+        else:
+            raise NameError(f'No account named {name} found')
 
     except ApiException as e:
         print("Exception when calling API: %s\n" % e)    
 
-def create_user(name):
+def delete_account(name):
     scope_id = kapua_config.get_scope_id()
+    try:
+        api_client = kapua_config.get_api_client()
+        api_instance = swagger_client.AccountsApi(api_client)
+
+        account_id = get_account_id(name)
+        api_instance.account_delete(scope_id, account_id)
+
+    except ApiException as e:
+        print("Exception when calling API: %s\n" % e)
+
+def create_user(account_id, name):
     try:
         api_client = kapua_config.get_api_client()
         api_instance = swagger_client.UsersApi(api_client)
 
         body = swagger_client.UserCreator(name=name)
-        api_response = api_instance.user_create(scope_id, body)
+        api_response = api_instance.user_create(account_id, body)
         pprint(api_response)
+
+        print(f'Created user {name} for account {account_id} with id: {api_response.id}')
+        return api_response.id
 
     except ApiException as e:
         print("Exception when calling API: %s\n" % e)
 
-def delete_user(name):
-    scope_id = kapua_config.get_scope_id()
-
+def delete_user(account_id, name):
     try:
         api_client = kapua_config.get_api_client()
         api_instance = swagger_client.UsersApi(api_client)
 
-        api_response = api_instance.user_simple_query(scope_id, name=name, limit=1)
+        api_response = api_instance.user_simple_query(account_id, name=name, limit=1)
 
         if len(api_response.items) > 0:
             user_id = api_response.items[0].id
-            pprint(f'Found user id: {user_id}')
-            api_instance.user_delete(scope_id, user_id)
+            print(f'Found user id: {user_id}')
+            api_instance.user_delete(account_id, user_id)
+        else:
+            print(f'No user named {name} found for account {account_id}')
 
     except ApiException as e:
         print("Exception when calling API: %s\n" % e)
 
-def list_users(scope_id=None):
-    if not scope_id:
-        scope_id = kapua_config.get_scope_id()
+def list_users(account_id=None):
+    if not account_id:
+        account_id = kapua_config.get_scope_id()
 
     try:
         api_client = kapua_config.get_api_client()
@@ -75,14 +98,18 @@ def list_users(scope_id=None):
 
         body = swagger_client.UserQuery()
 
-        api_response = api_instance.user_query(scope_id, body)
+        api_response = api_instance.user_query(account_id, body)
         pprint(api_response)
 
     except ApiException as e:
         print("Exception when calling API: %s\n" % e)
 
 if __name__ == '__main__':
-    #create_account(name='diec', organization='iss', email='diec@iss.edu')
-    list_accounts()
-    create_user(name='user1')
-    list_users()
+    #account_id = create_account(name='diec', organization='iss', email='diec@iss.edu')
+
+    account_id = get_account_id(name='diec')
+    #create_user(account_id=account_id, name='user1')
+    #list_users(account_id=account_id)
+
+    #delete_user(account_id=account_id, name='user1')
+    #delete_account(name='diec')
