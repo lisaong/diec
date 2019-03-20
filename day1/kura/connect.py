7b0c800cf66da71f7f673f6a50914f2bff942a80
Lisa Ong <lisaong@nus.edu.sg>
2019-03-20 08:34:12 +0000

    configurable port and url

--- day1/kura/connect.py
+++ day1/kura/connect.py
@@ -6,9 +6,6 @@ import json
 import kurapayload_pb2
 from google.protobuf import json_format
 
-# TODO: update this to the Kapua MQTT server hostname & port
-hostname='0.tcp.ngrok.io'
-port=11418
 client_id='02:42:AC:11:00:02'
 
 # default username and password created on Kapua,
@@ -16,7 +13,7 @@ client_id='02:42:AC:11:00:02'
 username='user1'
 password='Kapu@12345678'
 
-def send_message(subtopic, json_payload):
+def send_message(hostname, port, subtopic, json_payload):
     topic='$EDC/diec1/' + client_id + '/MQTT/' + subtopic
 
     pb = json_format.Parse(json.dumps(json_payload),
@@ -28,7 +25,7 @@ def send_message(subtopic, json_payload):
         auth={'username':username, 'password':password}, tls=None,
         protocol=mqtt.MQTTv311, transport='tcp')
 
-def send_event(event):
+def send_event(hostname, port, event):
     topic='$EDC/diec1/' + client_id + '/MQTT/' + event
 
     # Event,CONNECT,,DeviceId,02:42:AC:11:00:02,,Username,user1
@@ -41,32 +38,37 @@ def send_event(event):
         auth={'username':username, 'password':password}, tls=None,
         protocol=mqtt.MQTTv311, transport='tcp')
 
-def disconnect():
+def disconnect(hostname, port):
     with open('disconnect_template.json') as f:
         payload = json.load(f)
     print(payload)
 
-    send_event('DISCONNECT')
-    send_message('DC', payload)
+    send_event(hostname, port, 'DISCONNECT')
+    send_message(hostname, port, 'DC', payload)
 
-def connect(subtopic='BIRTH'):
+def connect(hostname, port):
     with open('connect_template.json') as f:
         payload = json.load(f)
     print(payload)
 
-    send_message('BIRTH', payload)
-    send_event('CONNECT')
+    send_message(hostname, port, 'BIRTH', payload)
+    send_event(hostname, port, 'CONNECT')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Sends a Kapua connection / disconnection request')
     parser.add_argument('command', type=str, choices=['connect', 'disconnect'])
+    parser.add_argument('hostname', type=str, help='Kapua MQTT url hostname, e.g. 0.tcp.ngrok.io')
+    parser.add_argument('port', type=int, help='Kapua MQTT url port, e.g. 11418')
 
     args = parser.parse_args()
 
+    hostname = args.hostname
+    port = args.port
+
     if args.command == 'connect':
-        connect()
+        connect(hostname, port)
     elif args.command == 'disconnect':
-        disconnect()
+        disconnect(hostname, port)
 
 
 
