3f3fcbd7ab062dcd9e203aab0143ab10bd2e8c60
Lisa Ong <lisaong@nus.edu.sg>
2019-03-20 06:01:24 +0000

    connect skeleton

--- day1/kura/connect.py
+++ day1/kura/connect.py
@@ -1,8 +1,28 @@
+import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
+import getmac
 import argparse
 
+# TODO: update this to the Kapua MQTT server hostname & port
+hostname='0.tcp.ngrok.io'
+port=11418
+
+# default username and password created on Kapua,
+# associated with this device
+username='user1'
+password='Kapu@12345678'
+
+# assume we are running the docker version of kapu
+mac=getmac.get_mac_address(interface='docker0')
+
 def connect():
-    pass
+    # send a message to connect
+    topic=f'$EDC/diec1/{mac}/MQTT/BIRTH'
+
+    publish.single(topic, payload='test', qos=0, retain=False,
+        hostname=hostname, port=port, keepalive=60, will=None,
+        auth={'username':username, 'password':password}, tls=None,
+        protocol=mqtt.MQTTv311, transport='tcp')
 
 def disconnect():
     pass
