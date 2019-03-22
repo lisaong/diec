62813667f6ceaa9f7695bc3beb2f8db41fe6e8f7
Lisa Ong <lisaong@nus.edu.sg>
2019-03-22 05:50:23 +0000

    basic iota client

---
+++ day2/iota_client.py
@@ -0,0 +1,26 @@
+# References:
+# https://github.com/Hribek25/IOTA101/blob/master/01_IOTA%20Essentials.ipynb
+
+import iota
+from iota.crypto.addresses import AddressGenerator
+from iota.crypto.types import Seed
+from pprint import pprint
+import time
+
+node_url = 'https://nodes.thetangle.org:443'
+security_level = 2
+
+def generate_seed():
+    return Seed.random()
+
+def generate_addresses(seed, count):
+    generator = AddressGenerator(seed=seed, security_level=security_level)
+    return generator.get_addresses(0, count) # index, count
+
+start = time.time()
+seed = generate_seed()
+print('Seed:', seed)
+addresses = generate_addresses(seed, 2)
+pprint(addresses)
+end = time.time()
+print('Elapsed Time:', end - start, 's')
