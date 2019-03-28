b5b807b22e8452bd15b0b008126959f758fc80d2
Lisa Ong <issohl@nus.edu.sg>
2019-03-28 16:41:53 +0800

    transmit sensor readings

--- day2/microbit/device_code.py
+++ day2/microbit/device_code.py
@@ -1,12 +1,16 @@
 from microbit import *
 
+# IOTA address is transmitted over serial
+# (in real life this needs to be replaced with an ID lookup)
+address = 'XQKBUNOERH9CJLLRQTNOLMWBJYUCGXORVNGEOEMBHNCPRVVBNSNNUOJMZODVUJXCOMMYXVLVNNJMBQMYX'
+bad_address = '999999999999999999999999999999999999BADBADBADBADBAD'
+
 while True:
     if button_a.is_pressed():
-        display.show(Image.HAPPY)
+        print(address)
     elif button_b.is_pressed():
-        display.show(Image.SAD)
+        print(bad_address)
 
-    print(accelerometer.current_gesture(), accelerometer.get_values(), temperature())
+    print(accelerometer.current_gesture(),
+          accelerometer.get_values(), temperature(), compass.heading())
     sleep(100)
-
-display.clear()
