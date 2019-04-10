a843b3366cc432febd7f96cdd396f8c0198c421b
Lisa Ong <issohl@nus.edu.sg>
2019-04-10 14:44:42 +0800

    handle invalid trytes

--- day2/feeder/iota_microservice.py
+++ day2/feeder/iota_microservice.py
@@ -25,9 +25,6 @@ class IotaMicroservice(MqttMicroservice):
 
         self.wallets = json.load(open('iota_wallets.json', 'r'))
         MqttMicroservice.__init__(self, channels)
-        
-        # unit test
-        self.on_message('test', {'vitamin A': 50, 'vitamin D3': 10, 'omega-3': 30, 'omega-6': 13, 'lysine': 15, 'id': '123'})
 
     def on_message(self, topic, payload):
         """Specialised message handler for this service"""
@@ -48,12 +45,18 @@ class IotaMicroservice(MqttMicroservice):
         return reduce(lambda x, y: x + y, nutrients)
 
     def do_transaction(self, id, cost):
-        result = None
-        for sender in self.wallets['birds']:
-            if sender['id'] == id:
-                bundle_hash = iota_client.do_transaction(sender['seed'],
-                    self.wallets['dispenser'], cost, message=id)
-                break
+        bundle_hash = None
+        try:
+            for sender in self.wallets['birds']:
+                if sender['id'] == id:
+                    bundle_hash = iota_client.do_transaction(sender['seed'],
+                        self.wallets['dispenser'], cost, message=id)
+                    break
+
+        except Exception as e:
+            # Seed may be invalid
+            print('Exception:', e)
+
         return bundle_hash
 
 if __name__ == '__main__':
