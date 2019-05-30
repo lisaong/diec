4fec4316083c7764db60db0e2ecadd83210f37e9
Lisa Ong <lisaong@nus.edu.sg>
2019-05-30 09:41:08 +0100

    WIP: reading from file

--- day3/inference/datasource.cc
+++ day3/inference/datasource.cc
@@ -1,12 +1,47 @@
 /* Functions for getting test data for TensorFlow Lite evaluation
+
+   Reference: https://www.geeksforgeeks.org/csv-file-management-using-c/
 */
 #include "datasource.h"
 
+#include <string>
+#include <sstream>
+#include <fstream>
+
+const char* kTestDataFile = "test.csv";
+
+using namespace std;
+
 namespace datasource {
 
-std::vector<std::vector<float>> GetData(int rows)
+vector<vector<float>> GetData(int rows)
 {
-    std::vector<std::vector<float>> result;
+    vector<std::vector<float>> result;
+
+    fstream fin;
+    fin.open(kTestDataFile, fstream::in);
+
+    vector<float> row;
+    string line, word, temp;
+
+    fin >> temp; // skip the header
+    int count = 0;
+
+    while (fin >> temp && count < rows) {
+        row.clear();
+
+        getline(fin, line);
+
+        stringstream s(line);
+
+        while(getline(s, word, ',')) {
+            row.push_back(stof(word));
+        }
+
+        result.push_back(row);
+        count++;
+    }
+
     return result;
 }
 
