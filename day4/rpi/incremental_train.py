00bb72766fe5bca95f1ee584452d8d83837fb468
Ong Hwei-Min Lisa <issohl@nus.edu.sg>
2019-09-24 13:41:02 +0800

    WIP: wrapped training into a function

--- day4/rpi/incremental_train.py
+++ day4/rpi/incremental_train.py
@@ -11,6 +11,9 @@ import tensorflow.keras as keras
 import numpy as np
 import pickle
 
+# parallelise collection and training
+from dask import compute, delayed
+
 class OnlineTrainer(serialio.SerialIoBase):
     def __init__(self, *args, **kw):
         '''https://github.com/pyserial/pyserial-asyncio/issues/41
@@ -24,25 +27,22 @@ class OnlineTrainer(serialio.SerialIoBase):
         #self.file.write('\n')
         #self.file.flush()
 
-def create_windows(X):
-    # ensure that dependencies are imported
-    import numpy as np
-  
-    # convert the time series so that each entry contains a series of timesteps.
-    # Before: rows, features
-    # After: rows, timesteps, features
-    timesteps = 20
+def create_windows(X, timesteps):
+    """convert the time series so that each entry contains a series of timesteps.
+    Before: rows, features
+    After: rows, timesteps, features
+    """  
     rolling_indexes = [(range(i, i+timesteps))
-                     for i in range(X.shape[0]-timesteps)]
+                        for i in range(X.shape[0]-timesteps)]
 
     X_out = np.take(X, rolling_indexes, axis=0)
-    
+
     return X_out
 
-def create_rolling_average(y):
-    # compute y based on rolling average of window values
-    # https://stackoverflow.com/questions/13728392/moving-average-or-running-mean
-    timesteps = 20
+def create_rolling_average(y, timesteps):
+    """compute y based on rolling average of window values
+    https://stackoverflow.com/questions/13728392/moving-average-or-running-mean
+    """
     y_out = np.convolve(y, np.ones((timesteps,))/timesteps, mode='valid')
 
     # shift forward by 1
@@ -53,11 +53,28 @@ def create_rolling_average(y):
 
     return y_out
 
+def update_model(model, preprocessors_and_data, X, y):
+    """Updates a model using the input data
+    """
+    timesteps = 20
+    X_scaled = preprocessors_and_data['scaler'].transform(X)
+
+    X_train = create_windows(X_scaled, timesteps)
+    y_train = create_rolling_average(y, timesteps)
+
+    X_val = preprocessors_and_data['X_val']
+    y_val = preprocessors_and_data['y_val']
+
+    model.fit(X_train, y_train, epochs=1, validation_data=(X_val, y_val))
+
 parser = argparse.ArgumentParser(description='Online training using data from micro:bit')
 parser.add_argument('comport', help='serial port for micro:bit')
 args = parser.parse_args()
 #print(args.comport)
 
+# A model trained using Keras with Tensorflow backend can be
+# loaded using tensorflow.keras
+# This avoids having to compile and install Keras on the Raspberry Pi 
 checkpoint = keras.models.load_model('./cnn_online.h5')
 print(checkpoint.summary())
 
@@ -73,9 +90,5 @@ X_sample = sample.loc[:, sample.columns != 'Gesture'].values
 y_sample = np.where(sample['Gesture'], 1, 0)
 # end temp
 
-X_sample_scaled = for_training['scaler'].transform(X_sample)
-X_sample_train = create_windows(X_sample_scaled)
-y_sample_train = create_rolling_average(y_sample)
+update_model(checkpoint, for_training, X_sample, y_sample)
 
-checkpoint.fit(X_sample_train, y_sample_train, epochs=1,
-               validation_data=(for_training['X_val'], for_training['y_val']))
