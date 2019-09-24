c2157a5878c6c9790853dbac5b21150d1574b577
Ong Hwei-Min Lisa <issohl@nus.edu.sg>
2019-09-24 11:41:58 +0800

    reorg

--- day4/acquire_data.py
+++ day4/acquire_data.py
@@ -6,9 +6,6 @@
 # import helper classes for acquiring data from the Micro:bit
 import serialio
 
-# Refer to the documentation below on how to find the serial port for the micro:bit
-# https://support.microbit.org/support/solutions/articles/19000022103-outputing-serial-data-from-the-micro-bit-to-a-computer
-
 comport = 'COM3' # Example Windows port
 # comport = '/dev/cu.usbmodem144102' # Example MacOS port
 
