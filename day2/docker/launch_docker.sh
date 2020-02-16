244764a869974a8d31c04eaec82521099738e48c
Srinivas Reddy <mrsrinivas@users.noreply.github.com>
2020-02-16 22:01:50 +0800

    passing the env variable IOTA_TANGLE_URL
    Passing the IOTA_TANGLE_URL variable if already set. `.env` is hardcoded now.

--- day2/docker/launch_docker.sh
+++ day2/docker/launch_docker.sh
@@ -3,5 +3,6 @@
 # https://vsupalov.com/docker-arg-env-variable-guide/
 docker run -it --privileged \
     -v ~/diec:/code \
-    --env-file=.env \
+    -e IOTA_TANGLE_URL=${IOTA_TANGLE_URL:-http://your_ec2_url:14265} \
+    -e MICROBIT_PORT=/dev/ttyACM0 \     
     lisaong/rpi-buster-pyota:1.0
