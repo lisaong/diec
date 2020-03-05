ce913157f1f12dc069284ae8201e860ac29d25a6
Lisa Ong <lisaong@nus.edu.sg>
2020-03-05 09:47:21 +0800

    random not a installable dependency

--- day4/rl/gym-fryshome/setup.py
+++ day4/rl/gym-fryshome/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_fryshome',
       version='0.0.1',
-      install_requires=['gym', 'numpy', 'random'] # dependencies
+      install_requires=['gym', 'numpy'] # dependencies
 )
