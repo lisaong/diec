25cf3b70985ac082277beabbb5639eadd69ac9a2
Lisa Ong <lisaong@nus.edu.sg>
2020-03-05 09:54:31 +0800

    typo

--- day4/rl/gym-fryshome/gym_fryshome/envs/frys_home_env.py
+++ day4/rl/gym-fryshome/gym_fryshome/envs/frys_home_env.py
@@ -59,7 +59,7 @@ class FrysHomeEnv(gym.Env):
     """Reset the environment to an initial state"""
 
     # Randomly initialise the state
-    self.state = random.randint(0, self.self.num_rooms-1)
+    self.state = random.randint(0, self.num_rooms-1)
 
     # Return the observation (same as the state in our case)
     obs = self.state
